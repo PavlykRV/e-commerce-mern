6b7140e42f931cc0c69001bd798f7dd52e116421
Roman Pavlyk <roman.p@developerdynamics.com>
2024-04-18 22:15:37 +0300

    feat(server): add db connection;

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,1 +1,24 @@
-console.log('Hello developers');
+import { MongooseError } from 'mongoose';
+
+const express = require('express');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+const port = process.env.PORT || 5000;
+
+const app = express();
+
+mongoose
+  .connect(process.env.MONGO_URL)
+  .then(() => console.log('DBconnection successful'))
+  .catch((error: MongooseError) => console.log(error));
+
+app.get('/api/ping', () => {
+  console.log('test request is successful');
+});
+
+app.listen(port, () => {
+  console.log(`API server is up and running at ${port}`);
+});
