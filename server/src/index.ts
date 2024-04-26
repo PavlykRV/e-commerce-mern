678b2c9acf8e6b43e8346d9c1ea9f7419f21ea0a
Roman Pavlyk <roman.p@developerdynamics.com>
2024-04-26 19:09:43 +0300

    feat(routes/checkout): add Stripe & payment route

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,11 +1,13 @@
 import express, { Express } from 'express';
 import mongoose, { MongooseError } from 'mongoose';
 import dotenv from 'dotenv';
+import cors from 'cors';
 import { router as userRoute } from './routes/user';
 import { router as authRoute } from './routes/auth';
 import { router as productRoute } from './routes/product';
 import { router as orderRoute } from './routes/order';
 import { router as cartRoute } from './routes/cart';
+import { router as checkoutRoute } from './routes/stripe';
 
 dotenv.config();
 
@@ -20,11 +22,13 @@ mongoose
   .catch((error: MongooseError) => console.log(error));
 
 app.use(express.json());
+app.use(cors());
 app.use('/api/auth/', authRoute);
 app.use('/api/users', userRoute);
 app.use('/api/products', productRoute);
 app.use('/api/orders', orderRoute);
 app.use('/api/carts', cartRoute);
+app.use('/api/checkout', checkoutRoute);
 
 app.listen(port, () => {
   console.log(`API server is up and running at ${port}`);
--- server/src/routes/stripe.ts
+++ server/src/routes/stripe.ts
@@ -1,5 +1,19 @@
 import { Router } from 'express';
+import Stripe from 'stripe';
+
+const stripe = new Stripe(process.env.STRIPE_KEY as string);
 
 const router = Router();
+
+router.post('/payment', (req, res) => {
+  stripe.charges
+    .create({
+      source: req.body.tokenId,
+      amount: req.body.amount,
+      currency: 'usd',
+    })
+    .then((response) => res.status(200).json(response))
+    .catch((err) => res.status(500).json(err));
+});
 
 export { router };
